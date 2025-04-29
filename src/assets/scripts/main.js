a5d4fe7104a83029112a7311c6fd5a2093c4d5ee
Alfonso J <alfonsojfp@gmail.com>
2025-04-29 08:11:55 +0200

    Apagon

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,12 +3,15 @@
  * see commented examples below
  */
 
+import * as bootstrap from 'bootstrap';
+
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
 /**
  * Write any other JavaScript below
  */
+
 
 +( function() {
   const university = "UOC";
