4b40421c13ce2d3ae5a21f8e87e0af073cf92b4b
Alfonso J <alfonsojfp@gmail.com>
2025-04-30 16:42:45 +0200

    Solucionando carga dinamica

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -13,27 +13,77 @@ import * as bootstrap from 'bootstrap';
  */
 
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
+const approot = document.getElementById("app");
+/*
+document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelectorAll('.submenu');
+  
+  links.forEach(link => {
+      link.addEventListener('click', event => {
+          event.preventDefault();            
+          const url = new URL(window.location.href);
+          // console.log('url', url.protocol);
+          const contentUrl = link.getAttribute('data-url');
+          // console.log("URL", contentUrl);
+          window.location = `${ url.protocol }//${ url.host }/detalle.html?det=${contentUrl}`;
+      });
+  });
+});
+*/
 
-  const container = document.getElementById("loadViews");
-  if(!container){
+
++( function() {
+
+
+  if(!approot){
     console.error("Error no existe el div");
     return;
   }
 
-  const view = async (nompage) => {
-    console.log('name',nompage);
-    try{
+//   document.addEventListener('DOMContentLoaded', () => {
+//     const links = document.querySelectorAll('.menuJS');
+//     console.log(links);
+    
+//     links.forEach(link => {
+//         link.addEventListener('click', event => {
+//             event.preventDefault();            
+//             const contentUrl = link.getAttribute('data-url');
+//             console.log("URL", link);
+//             // window.location = `${ url.protocol }//${ url.host }/detalle.html?det=${contentUrl}`;
+//         });
+//     });
+// });
+
+const menu = document.querySelector('.menuJS').children;
+
+console.log(menu);
+
+    menu.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault();            
+            const contentUrl = link.getAttribute('data-url');
+            console.log("URL", link);
+            // window.location = `${ url.protocol }//${ url.host }/detalle.html?det=${contentUrl}`;
+        });
+    });
+
+} )();
+
+
+export async function render(nompage) {
+  // Renderiza la página
+  console.log('exposr', nompage);
+
+  try{
       const ruta = `./views/pages/${ nompage }.html`;
       const response = await fetch(ruta);
       console.log('res',response);
       if(!response.ok) throw new Error('ERROR- No se ha podido cargar la página');
-      const htmlTXT = await response.content;
-      container.html( htmlTXT );
-    } catch (err){
+      const htmlTXT = await response.text();
+      console.log(htmlTXT);
+      
+      app.innerHTML = htmlTXT;
+  } catch (err){
       console.error(err);
-    }    
-  };
-} )();
+  }            
+}
