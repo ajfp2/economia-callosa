223fc1b448c4ebd221a4d332d159d4f27b37accb
Alfonso J <alfonsojfp@gmail.com>
2025-05-11 09:22:48 +0200

    Arreglos JS para toast

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -14,10 +14,6 @@ import { contactJS } from './contact';
  * Write any other JavaScript below
 */
 
-
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
   const links = document.querySelectorAll('.navmenu');        
   links.forEach(link => {
@@ -38,31 +34,61 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const handleSubmit = event => {
   event.preventDefault();
-  console.log("Form Contact", event);
 
   const myForm = event.target;
   const formData = new FormData(myForm);
-
-  const toastLiveExample = document.getElementById('liveToast');
+  const btn = document.getElementById("submitForm");
+  btn.disabled = true;
+  showToast("Enviando datos", "secondary", ['top-0','end-0']);
 
   fetch("/", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams(formData).toString()
-  })
-    .then(() => {
-      alert("Thank you for your submission")       
-      const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-      toastBootstrap.show();                        
+  }).then(() => {    
+      showToast("Formulario enviado correctamente", "success", ['top-0','end-0']);
+      btn.disabled = false;
     })
     .catch(error => {
-      alert(error)
+      console.error("ERROR", error);
+      showToast("Ha ocurrido un error, vuelva a intentarlo", "danger", ['top-0','end-0']);
+      btn.disabled = false;
     });
 };
 
-// const form = document.querySelector("form");
-// console.log("Contact JS", form);
-// form.addEventListener("submit", handleSubmit);
+document.addEventListener("submit", function (event) {
+  if (event.target.matches("form")) {
+    handleSubmit(event);
+  }
+});
 
+document.addEventListener("change", function (event) {
+  if (event.target.matches("input[name=politicaCH]")) {
+    const btn = document.getElementById("submitForm");
+    if(event.target.checked == false) btn.disabled = true;
+    else btn.disabled = false;
+  }
+});
+
+function showToast(content, type, position) {
+    var delay = 15000;
+    position.forEach((el) => {
+        document.querySelector("#toast-container").classList.add(el);
+    });
+    var html = `<div class="toast align-items-center text-white bg-${type} border-0" role="alert" aria-live="assertive" aria-atomic="true"><div class="d-flex"><div class="toast-body h6 p-3 m-0">${content}</div><button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button></div></div>`;
+    var toast = htmlToElement(html);
+    var toastContainer = document.querySelector("#toast-container");
+    toastContainer.appendChild(toast);
+    var toast = new bootstrap.Toast(toast, {delay:delay, animation:true});
+    toast.show();
+    //setTimeout(() => toast.remove(), delay + 15000);
+}
+
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim();
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
 
 
